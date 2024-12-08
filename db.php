280dbee1b3eb02ccc42c3bf733c49f2de9286769
Pavel Osminin <osperevod@gmail.com>
2024-12-09 01:25:44 +0500

    fix typo

--- db.php
+++ db.php
@@ -25,7 +25,7 @@ $sql = "CREATE TABLE IF NOT EXISTS users(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
-    pass VARCHAR(50) NOT NULL
+    password VARCHAR(50) NOT NULL
 )";
 
 if (!$mysqli_query($link, $sql)) {
