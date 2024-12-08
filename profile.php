c2fe006fb260d1be01b2d077470491e3da977f5e
Pavel Osminin <osperevod@gmail.com>
2024-12-09 00:07:39 +0500

    initial version from task42_ascs4 repo

---
+++ profile.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Осминин П.Г.</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css” />
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+    <div class="container nav_bar">
+        <div class="row">
+            <div class="col-3 nav_logo"></div>
+            <div class="col-9">
+                
+                <div class="nav_text">Обо мне</div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            
+            <div class="col-8">
+                
+                <h2>
+                    Павел Осминин интересуется информационной безопасностью.<br>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus feugiat vel libero nec fringilla.
+                    Aliquam imperdiet iaculis mi ac dapibus. Praesent nec tempus ligula, et sodales orci.
+                    Nam ultrices sit amet mauris sit amet elementum. Vestibulum suscipit, orci a facilisis luctus, mi sem tincidunt lorem,
+                    eu mattis neque nisi id quam. Suspendisse vel magna ante. Integer iaculis quam at egestas finibus.
+                    Donec fringilla, mi dapibus commodo volutpat, tortor quam dignissim magna, nec tempor quam nisl non dui.
+                    Nam sit amet massa vitae tortor feugiat euismod eget in nulla. Proin nibh neque, euismod ac dui at, mollis sodales risus. 
+                </h2>
+            </div>
+                <div class="col-4">
+                    <div class="row my_photo"></div>
+                    <div class="row"><p class="title_photo">Осминин П.Г.</p></div>
+                </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="button_js col-12">
+                <button id="myButton">Click me</button>
+                <p id="demo"></p></div>
+        </div>
+    </div>
+    <script type="text/javascript" src="js/button.js"></script>
+</body>
+</html>
